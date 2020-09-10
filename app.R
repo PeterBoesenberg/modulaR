1bddaa015dbdcad89d7bcadb4c5de0883059a77f
Peter Bösenberg <peterboesenberg@gmail.com>
2020-09-10 21:03:45 +0200

    lintr config with all linters

--- app.R
+++ app.R
@@ -1,12 +1,12 @@
-getModule <- function() {
+get_module <- function() {
   # private module variable
-  data <- c(12,3,54,5)
+  data <- c(12L, 3L, 54L, 5L)
   # private module function
   calculate_something <- function() {
     return(sd(data))
   }
-  
-  
+
+
   env <- new.env()
   #public functions
   env$create <- function() {
